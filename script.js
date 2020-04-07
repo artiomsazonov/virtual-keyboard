1d861d609f39e30a4dc36484dd68150dd2458ac4
artiomsazonov <sazonau.artiom@yandex.by>
2020-04-07 21:35:44 +0300

    feat: mouse response

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ let container = document.querySelector('.wrapper');
 let textarea = document.createElement('textarea');
 container.append(textarea)
 let divKeyboard = document.createElement('div');
-divKeyboard.setAttribute( "id", "keyboard" );
+divKeyboard.setAttribute("id", "keyboard");
 
 container.append(divKeyboard)
-let keyboard = [96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, "Backspace", "Tab", 113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 92, 'Del', "Caps Lock", 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39, 13, 'Shift', 122, 120, 99, 118, 98, 110, 109, 44, 46, 47, '^', 'Shift', "Ctrl", 'Win', 'Alt', 32, 'Alt', '<', "^", ">","Ctrl"]
+let keyboard = [96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, "Backspace", "Tab", 113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 92, 'Del', "Caps Lock", 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39, 13, 'Shift', 122, 120, 99, 118, 98, 110, 109, 44, 46, 47, 'arrowTop', 'Shift', "Ctrl", 'Win', 'Alt', 32, 'Alt', 'arrowLeft', "arrowBottom", "arrowRight", "Ctrl"]
 let keyb = [];
 document.onkeypress = function (event) {
     keyb.push(event.keyCode);
@@ -36,8 +36,8 @@ function init() {
             case 'Shift':
                 out += "<div class='k-key key_shift key_spec' data='" + keyboard[i] + "'> <p>" + 'Shift' + "</p></div>";
                 continue;
-            case '^':
-                out += "<div class='k-key key_arrowTop key_spec' data='" + keyboard[i] + "'> <p>" + '^' + "</p></div>";
+            case 'arrowTop':
+                out += "<div class='k-key key_arrowTop key_spec' data='" + keyboard[i] + "'> <p>" + '&#9650;' + "</p></div>";
                 continue
             case "Ctrl":
                 out += "<div class='k-key key_ctrl key_spec' data='" + keyboard[i] + "'> <p>" + "Ctrl" + "</p></div>";
@@ -48,14 +48,14 @@ function init() {
             case 'Alt':
                 out += "<div class='k-key key_alt key_spec' data='" + keyboard[i] + "'> <p>" + 'Alt' + "</p></div>";
                 continue;
-            case '<':
-                out += "<div class='k-key key_arrowLeft key_spec' data='" + keyboard[i] + "'> <p>" + '<' + "</p></div>";
+            case 'arrowLeft':
+                out += "<div class='k-key key_arrowLeft key_spec' data='" + keyboard[i] + "'> <p>" + '&#9668;' + "</p></div>";
                 continue
-            case "^":
-                out += "<div class='k-key key_arrowBottom key_spec' data='" + keyboard[i] + "'> <p>" + "^" + "</p></div>";
+            case "arrowBottom":
+                out += "<div class='k-key key_arrowBottom key_spec' data='" + keyboard[i] + "'> <p>" + "&#9660;" + "</p></div>";
                 continue
-            case ">":
-                out += "<div class='k-key key_arrowRight key_spec' data='" + keyboard[i] + "'> <p>" + ">" + "</p></div>";
+            case "arrowRight":
+                out += "<div class='k-key key_arrowRight key_spec' data='" + keyboard[i] + "'> <p>" + "&#9658;" + "</p></div>";
                 continue
             case 32:
                 out += "<div class='k-key key_space' data='" + keyboard[i] + "'> <p>" + String.fromCharCode(keyboard[i]) + "</p></div>";
@@ -68,33 +68,55 @@ function init() {
     document.getElementById('keyboard').innerHTML = out;
 }
 init();
-
-document.onkeypress = function (event) {
-    console.log(event.keyCode)
-    document.querySelectorAll(".k-key").forEach(function (element) {
-        element.classList.remove("activ")
-    });
-    document.querySelector("#keyboard .k-key[data='" + event.keyCode + "']").classList.add("activ")
-}
+document.querySelectorAll(".k-key").forEach(function (element) {
+    document.onkeypress = function (event) {
+        document.querySelectorAll(".k-key").forEach(function (element) {
+            element.classList.remove("activ")
+        });
+        document.querySelector("#keyboard .k-key[data='" + event.keyCode + "']").classList.add("activ")
+    }
+})
 document.querySelectorAll(".k-key").forEach(function (element) {
     element.onclick = function (event) {
-        document.querySelectorAll(".k-key").forEach(function (element) {
+        const textarea = document.querySelector('textarea')
+        const keys = document.querySelectorAll(".k-key")
+        keys.forEach(function (element) {
             element.classList.remove("activ");
+
         });
-        let code = this.getAttribute('data');
         this.classList.add('activ')
-        document.querySelector('textarea').insertAdjacentText('beforeend', this.textContent)
+        switch (this.outerText) {
+            case "Enter":
+                textarea.value += '\n'
+                break;
+            case "Space":
+            case "Backspace":
+                textarea.setRangeText('', textarea.selectionStart-1, textarea.selectionEnd)
+                break;
+            case "Tab":
+                textarea.value += "   ";
+                break;
+            case 'Del':
+                textarea.setRangeText('', textarea.selectionStart, textarea.selectionEnd + 1)
+                break;
+            case "Caps Lock":
+            case 'Shift':
+            case 'arrowTop':
+            case "Ctrl":
+            case 'Win':
+            case 'Alt':
+            case "arrowLeft":
+            case "arrowBottom":
+            case "arrowRight":
+            case "":
+                textarea.value += " ";
+                break;
+            default:
+                textarea.value += this.outerText;
+        }
     }
 });
-function setFocus(){
+function setFocus() {
     document.querySelector("textarea").focus();
 }
 setFocus()
-// window.onload = function(){
-//     var items = document.querySelectorAll(".k-key")
-//     for(let i =0; i<items.length; i++){
-//         items[i].onclick = function(){
-//             this.
-//         }
-//     }
-// }
